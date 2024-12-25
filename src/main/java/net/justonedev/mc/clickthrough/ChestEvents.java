94e2f09a6a85a646250a0f635f392f18a058fd26
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2024-12-25 21:00:41 +0100

    Created plugin

---
+++ src/main/java/net/justonedev/mc/clickthrough/ChestEvents.java
@@ -0,0 +1,64 @@
+package net.justonedev.mc.clickthrough;
+
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.BlockInventoryHolder;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
+
+import java.util.UUID;
+
+public class ChestEvents implements Listener {
+
+    Clickthrough plugin;
+
+    public ChestEvents(Clickthrough plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onClick(final PlayerInteractAtEntityEvent e) {
+        if (e.getRightClicked().getType() != EntityType.ITEM_FRAME && e.getRightClicked().getType() != EntityType.GLOW_ITEM_FRAME) return;
+        if (e.getPlayer().isSneaking()) return;
+        ItemFrame frame = (ItemFrame) e.getRightClicked();
+        Block attached = frame.getLocation().getBlock().getRelative(frame.getAttachedFace());
+        if (!(attached.getState() instanceof BlockInventoryHolder holder)) return;
+
+        //*
+        // Cancelling the event does not actually cancel the interaction
+        // So, we freeze the frame for one (1) tick
+        if (!frame.isFixed()) {
+            final String metadata = UUID.randomUUID().toString();
+            frame.setMetadata("clickthrough-unfreeze", new FixedMetadataValue(plugin, metadata));
+            frame.setFixed(true);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+                if (frame.hasMetadata("clickthrough")) {
+                    // Some other thread "reserved" it and is going to unfreeze it
+                    // This is basically in case two people click on the chest at the same time
+                    if (!frame.getMetadata("clickthrough").getFirst().asString().equals(metadata)) return;
+                }
+                frame.setFixed(false);
+            }, 1);
+        }//*/
+
+
+
+        /*
+        // Cancelling the event does not actually cancel the interaction
+        // So, we freeze the frame for one (1) tick
+        if (!frame.isFixed()) {
+            frame.setFixed(true);
+            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> frame.setFixed(false), 1);
+        }*/
+
+
+        e.setCancelled(true);
+        e.getPlayer().openInventory(holder.getInventory());
+    }
+
+}
---
+++ src/main/java/net/justonedev/mc/clickthrough/Clickthrough.java
@@ -0,0 +1,20 @@
+package net.justonedev.mc.clickthrough;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Clickthrough extends JavaPlugin {
+
+    public static boolean enabled = true;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(new ChestEvents(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
