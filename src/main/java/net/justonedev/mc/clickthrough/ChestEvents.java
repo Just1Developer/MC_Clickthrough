e76f934c5bebaed0e9fbb22ed3a47bf2d4693f77
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2024-12-26 13:24:22 +0100

    Added signs

--- src/main/java/net/justonedev/mc/clickthrough/ChestEvents.java
+++ src/main/java/net/justonedev/mc/clickthrough/ChestEvents.java
@@ -2,11 +2,14 @@ package net.justonedev.mc.clickthrough;
 
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.type.WallSign;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.BlockInventoryHolder;
 
 public class ChestEvents implements Listener {
@@ -18,7 +21,7 @@ public class ChestEvents implements Listener {
     }
 
     @EventHandler
-    public void onClick(final PlayerInteractAtEntityEvent e) {
+    public void onClickItemFrame(final PlayerInteractAtEntityEvent e) {
         if (e.getRightClicked().getType() != EntityType.ITEM_FRAME && e.getRightClicked().getType() != EntityType.GLOW_ITEM_FRAME) return;
         if (e.getPlayer().isSneaking()) return;
         ItemFrame frame = (ItemFrame) e.getRightClicked();
@@ -31,6 +34,21 @@ public class ChestEvents implements Listener {
             frame.setFixed(true);
             Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> frame.setFixed(false), 1);
         }
+
+        e.setCancelled(true);
+        e.getPlayer().openInventory(holder.getInventory());
+    }
+
+    @EventHandler
+    public void onClickSign(final PlayerInteractEvent e) {
+        if (!e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
+        Block rightClicked = e.getClickedBlock();
+        if (rightClicked == null) return;   // To appease IntelliJ
+
+        if (!(rightClicked.getState() instanceof WallSign)) return;
+        if (e.getPlayer().isSneaking()) return;
+        Block attached = rightClicked.getLocation().getBlock().getRelative(((WallSign) rightClicked).getFacing().getOppositeFace());
+        if (!(attached.getState() instanceof BlockInventoryHolder holder)) return;
 
         e.setCancelled(true);
         e.getPlayer().openInventory(holder.getInventory());
